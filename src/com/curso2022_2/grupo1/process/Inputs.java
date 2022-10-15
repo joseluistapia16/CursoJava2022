c8c136055fb4a7a22683fb55b4bc10ffdc959eb6
JOSELUIS77 <jtapia@itsgg.edu.ec>
2022-10-15 16:05:45 -0500

    Conformacion de grupos , 1 proceso

---
+++ src/com/curso2022_2/grupo1/process/Inputs.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curso2022_2.grupo1.process;
+
+/**
+ *
+ * @author josel
+ */
+public class Inputs {
+    
+}
