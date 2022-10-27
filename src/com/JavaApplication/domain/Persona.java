aeee86660d090c78b172ad7a1836d2503fdc392a
FRANKLIN RV <FRANKLIN RV@Frankl1nr10fr1o.cpe.satnet.net>
2022-10-26 22:47:43 -0500

    dominio persona

--- src/com/JavaApplication/domain/Persona.java
+++ src/com/JavaApplication/domain/Persona.java
@@ -14,7 +14,53 @@ abstract public class Persona {
     private String nombre;
     private String apellido;
     private String correo;
+
+    public Persona() {
+
+    }
+
+    public Persona(String cedula, String nombre, String apellido, String correo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getData() {
+
+        return this.cedula + " " + this.nombre + " "
+                + this.apellido + " " + this.correo;
+    }
 }
-
-    
-
