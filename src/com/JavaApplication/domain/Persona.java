ddd233cb7f31b0b81039af286badc7ac0a2207ca
FRANKLIN RV <FRANKLIN RV@Frankl1nr10fr1o.cpe.satnet.net>
2022-10-26 22:40:52 -0500

    dominio

---
+++ src/com/JavaApplication/domain/Persona.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.JavaApplication.domain;
+
+/**
+ *
+ * @author FRANKLIN RV
+ */
+abstract public class Persona {
+
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String correo;
+}
+
+    
+
